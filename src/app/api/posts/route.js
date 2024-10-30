e6218cb33e891a5e5e332055a95e6f2bad72f070
royanlord <royanfarid32@gmail.com>
2024-10-30 11:52:49 +0700

    create post method to insert data

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -17,3 +17,27 @@ export async function GET() {
         }
     );
 }
+
+export async function POST(request) {
+    // get all request
+    const { title, content } = await request.json()
+
+    // create data post
+    const post = await prisma.post.create({
+        data: {
+            title: title,
+            content: content
+        }
+    })
+
+    return NextResponse.json(
+        {
+            success: true,
+            message: "Post created successfully!",
+            data: post
+        },
+        {
+            status: 201
+        }
+    )
+}
