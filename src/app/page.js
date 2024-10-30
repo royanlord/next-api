2542f3f3a1edc0dee573308c81e29beb80784245
royanlord <royanfarid32@gmail.com>
2024-10-30 16:21:43 +0700

    clear globals.css n clear default data

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
+      {/* <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.js</code>
@@ -89,7 +89,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
   )
 }
